4e503ae40a4f01a4718a8052c84289e8a57b8f7b
muhai <muhai.li@tuya.com>
2023-06-01 17:32:25 +0800

    chore: update

--- src/api/api.ts
+++ src/api/api.ts
@@ -14,7 +14,7 @@ export const saveConfig = (config: AppConfig, input: string) => {
   const encodeData = Ptbk.encode(config, input)
   console.log(str, encodeData)
   window.open(
-    `https://github.com/Saber2pr/income-calc/new/master/public?filename=/public/config-${Date.now()}&value=${encodeURIComponent(
+    `https://github.com/Saber2pr/income-calc/new/master?filename=public/config-${Date.now()}&value=${encodeURIComponent(
       JSON.stringify(encodeData),
     )}`,
     '_blank',
